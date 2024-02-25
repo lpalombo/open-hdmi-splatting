c77b7727388a1185fde98df5bfa2488c5f841aab
lpalombo <lucp.xyz@gmail.com>
2024-02-24 19:19:17 -0500

    setup projection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
+import { Box, Environment, OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
 import { Splat, SplatMaterialType } from './Splat';
 import { ReactNode, forwardRef, useRef } from 'react';
 import { Matrix4 } from 'three';
 
 const matrix = new Matrix4();
+const HALF_PI = Math.PI / 2;
 
 function App() {
   const container = useRef<HTMLDivElement>(null!);
@@ -15,19 +16,39 @@ function App() {
         <div className="top-section">
           <PanelView className="panel panel-west">
             <Scene />
-            <PerspectiveCamera makeDefault position={[0, 0, 8]} />
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 0, 0]}
+              rotation={[0, HALF_PI * 2, 0]}
+              fov={48.98}
+            />
           </PanelView>
           <PanelView className="panel panel-north">
             <Scene />
-            <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 0, 0]}
+              rotation={[0, HALF_PI * 1, 0]}
+              fov={131.0123}
+            />
           </PanelView>
           <PanelView className="panel panel-east">
             <Scene />
-            <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 0, 0]}
+              rotation={[0, HALF_PI * 0, 0]}
+              fov={48.98}
+            />
           </PanelView>
           <PanelView className="panel panel-south">
             <Scene />
-            <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 0, 0]}
+              rotation={[0, HALF_PI * 3, 0]}
+              fov={131.0123}
+            />
           </PanelView>
         </div>
 
@@ -67,21 +88,25 @@ function Scene() {
   const group = useRef<THREE.Group>(null!);
 
   useFrame(({ clock }) => {
-    group.current.position.x = Math.sin(clock.getElapsedTime() * 0.1) * 5;
-    group.current.position.z = Math.cos(clock.getElapsedTime() * 0.1) * 5;
+    group.current.position.x = Math.sin(clock.getElapsedTime() * 2) * 5;
+    group.current.position.z = Math.cos(clock.getElapsedTime() * 2) * 5;
   });
 
   return (
     <>
       <color attach="background" args={['#171720']} />
       <ambientLight />
+      <Environment preset="sunset" background />
       <group matrixAutoUpdate={false} onUpdate={self => (self.matrix = matrix)}>
         <group ref={group}>
+          <Box args={[1, 1, 1]} position={[0, 0, 0]} />
+        </group>
+        {/* <group ref={group}>
           <Splat
             src="https://pub-c94e113880784f8f8227940d6abceeef.r2.dev/gs_Ichiban_Living.splat"
             position={[0, 0, 0]}
           />
-        </group>
+        </group> */}
       </group>
     </>
   );
